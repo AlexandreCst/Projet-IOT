b1718b4b9baf5d981f0511cc94318eb7e6fc60d7
Anaëlle Hervé <anaelle.herve@mines-albi.fr>
2022-01-26 09:59:20 +0100

    fusion travaux

--- src/App.js
+++ src/App.js
@@ -2,24 +2,124 @@ import logo from './logo.svg';
 import './App.css';
 import LightRoom from './LightRoom';
 import axios from 'axios';
+import React, {useState, useEffect} from 'react'
 
 function App() {
-  axios.post("http://172.23.11.52:5000/maison/light/3/0")
-  .then(function (response) {
-    console.log(response);
+
+      // On récupère l'état des lumières
 
 
-  });
-  return (
+      const req = async () => {
+        const response = await axios.get('https://dog.ceo/api/breeds/list/all')
+        console.log(response)
+      }
+      req() // Calling this will make a get request and log the response.
+      
+      // var etat = null ;
+
+      // axios.get("http://172.20.8.176:8080/maison/light")
+      // .then(function (response) {
+      //   console.log(response);
+      //   etat = response.data ;
+      //   setResult(response.data)
+      // });
+
+      // console.log("wololo")
+      // console.log(etat)
+
+
+      // etat = JSON.parse(etat)
+
+      // console.log('oui')
+      // console.log(etat)
+   
+      // cuis = etat.Cuisine;
+      // sal = etat['Salon'] ;
+
+      // console.log(yep)
+      // console.log(cuis)
+
+      
 
     
-    <div className="App">
-      <p>Ma Maison Connectée</p>
-      <LightRoom roomId="1" etat="on"></LightRoom>
-      <LightRoom roomId="2" etat="on"></LightRoom>
-      <LightRoom roomId="3" etat="off"></LightRoom>
-      <LightRoom roomId="4" etat="off"></LightRoom>
-    </div>
+    // // On récupère l'état des volets
+
+    // axios.get("http://172.20.8.176:5000/maison/rideaux")
+    // .then(function (response) {
+    // console.log(response);
+    // var etat = response.data ;
+    // });
+  
+  return (
+
+    <html>
+
+      <head>
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" 
+integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" 
+crossorigin="anonymous"/>
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+      </head>
+
+      <body>
+
+        {/* <button type="button" class="btn">vdkehdl</button> */}
+
+
+
+        <div className="App-header">
+          <p>Ma Maison Connectée</p>
+        </div>
+
+        {/* <h1 className="App-piece">{etat} </h1> */}
+
+
+        <div class="container">
+
+          <div class="row-2">
+            <br></br>
+          </div>
+
+          <div class="row justify-content-center">
+
+            <span class="border border-dark">
+              <div class="col">
+                <LightRoom roomId='1' name="Chambre 1" etat='mettre etat'></LightRoom>
+              </div>
+            </span>
+
+            <span class="border border-dark">
+              <div class="col">
+                <LightRoom roomId='2' name="Chambre 2" etat='mettre etat'></LightRoom>
+              </div>
+            </span>
+
+          </div>
+
+          <div class="row justify-content-center">
+
+            <span class="border border-dark">
+              <div class="col">
+                <LightRoom roomId='3' name="Cuisine" etat='mettre etat'></LightRoom>
+              </div>
+            </span>
+
+            <span class="border border-dark">
+              <div class="col">
+                <LightRoom roomId='4' name="Salon" etat='mettre etat'></LightRoom>
+              </div>
+            </span>
+
+          </div>
+
+        </div>
+
+      </body>
+
+    </html>
+
+    
   );
 }
 
--- src/LightRoom.jsx
+++ src/LightRoom.jsx
@@ -1,26 +1,43 @@
 import React from "react";
+import axios from 'axios';
 
 function LightRoom(props) {
-    console.log(props);
-    var pic = "toto";
 
-    if (props.etat === "on") {
-        pic = "titi";
+    //console.log(props);
+
+    console.log(props.roomId)
+
+
+    // Variable pour l'état des lumières dans la pièce
+
+    // etatlum = etat
+
+    // console.log('wesh')
+    // console.log(etatlum)
+
+    
+    // // Variable pour l'état des volets dans la pièce
+
+    var etatvolet = 'on';
+
+    var etatlum = 'on';
+
+    // etatvolet= etat['props.name'] ;
     
     return(
         <div>
-            <p>
-                Bonjour de Lightroom {props.roomId} {pic}
+            <h1 className="App-piece">{props.name} </h1>
+            <p class="text-center"> Lumière : {etatlum}
+            <br></br> Volets : {etatvolet} 
             </p>
+            <img src='https://media.istockphoto.com/vectors/light-bulb-with-rays-lighting-electric-lamp-creative-idea-solution-vector-id1149246206?k=20&m=1149246206&s=612x612&w=0&h=yz2GuTngOnZAHYq4wDsAwq0mp20OWlIUSSdmyIU3sRM=' width='100'></img>
+            <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQf2RlM1U8Neiv0EJ3JBUWg046KssJYF-Qt9Q&usqp=CAU' width='100'></img>
+           
+            {/* if etatlum == 'Allumée'
+            <button value = 'Eteindre'></button>
+            <button value = 'Allumer'></button> */}
         </div>
     )
-    }else{
-        return(
-            <div>
-                <p>popop</p>
-            </div>
-        )
-    }
 }
 
 export default LightRoom;
