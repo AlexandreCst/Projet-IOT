91b67d79955d363561434d545b67375cd6a601f5
Anaëlle Hervé <anaelle.herve@mines-albi.fr>
2022-01-28 11:41:19 +0100

    Bouton

--- src/App.js
+++ src/App.js
@@ -8,70 +8,43 @@ function App() {
 
       // On récupère l'état des lumières
 
-      //const req = async () => {
-        //const response = await axios.get('https://dog.ceo/api/breeds/list/all')
-        //console.log(response)
-      //}
-      //req() // Calling this will make a get request and log the response.
-      
-      // var etat = null ;
+      // useEffect nous permettra de déclencher le fetch; useState permettra de stocker le retour de l'API dans le state
 
-      // axios.get("http://172.20.8.176:8080/maison/light")
-      // .then(function (response) {
-      //   console.log(response);
-      //   etat = response.data ;
-      //   setResult(response.data)
-      // });
-
-      // console.log("wololo")
-      // console.log(etat)
-
-
-      // etat = JSON.parse(etat)
-
-      // console.log('oui')
-      // console.log(etat)
-   
-      // cuis = etat.Cuisine;
-      // sal = etat['Salon'] ;
-
-      // console.log(yep)
-      // console.log(cuis)
-
-      
-
-    
-    // // On récupère l'état des volets
-
-    // axios.get("http://172.20.8.176:5000/maison/rideaux")
-    // .then(function (response) {
-    // console.log(response);
-    // var etat = response.data ;
-    // });
-    //var ip = "http://172.20.8.176:8080/maison/"
-    var chambre1_URL = "http://172.20.8.176:8080/maison/light/3/1"; // Chambre 1
-    var chambre2_URL = "http://172.20.8.176:8080/maison/light/5/toggle"; // Chambre 2
-    var cuisine_URL = "http://172.20.8.176:8080/maison/light/7/toggle"; // Cuisine
-    var salon_URL = "http://172.20.8.176:8080/maison/light/11/toggle"; //Salon
-
-    axios.post(chambre1_URL)
-    .then(data => console.log(data))
-    .catch(data => console.log('error', data));
-
-    // useEffect nous permettra de déclencher le fetch; useState permettra de stocker le retour de l'API dans le state 
-      
-    const [etat, setEtat] = useState ([]) ;
+      const [etat, setEtat] = useState ([]) ;
 
       useEffect(() => {
-          fetch("http://172.23.11.52:8080/maison/light").then(response =>
+          fetch("http://172.20.8.176:5000/maison/light").then(response =>
             response.json().then(data => {
               setEtat(data);
               console.log('debut');
               console.log(etat);
             })
           );
-      },[]);
-  
+      },[]); 
+
+    
+    // // On récupère l'état des volets
+
+
+    //On récupère les URL pour la gestion des lumières
+
+    //var ip = "http://172.20.8.176:8080/maison/"
+    var chambre1_lon = "http://172.20.8.176:5000/maison/light/3/1"; // Chambre 1
+    var chambre2_lon = "http://172.20.8.176:5000/maison/light/5/1"; // Chambre 2
+    var cuisine_lon = "http://172.20.8.176:5000/maison/light/7/1"; // Cuisine
+    var salon_lon = "http://172.20.8.176:5000/maison/light/11/1"; //Salon
+
+    var chambre1_loff = "http://172.20.8.176:5000/maison/light/3/0"; // Chambre 1
+    var chambre2_loff = "http://172.20.8.176:5000/maison/light/5/0"; // Chambre 2
+    var cuisine_loff = "http://172.20.8.176:5000/maison/light/7/0"; // Cuisine
+    var salon_loff = "http://172.20.8.176:5000/maison/light/11/0"; //Salon
+
+    // axios.post(chambre2_URL)
+    // .then(data => console.log(data))
+    // .catch(data => console.log('error', data));   
+
+    // axios.post(cuisine_URL)
+
   return (
 
     <html>
@@ -96,7 +69,7 @@ crossorigin="anonymous"/>
             <br></br>
           </div>
 
-          <div class='col-md-2 offset-md-11 border border-dark'>
+          <div class='col-md-2 offset-md-11 border border-dark' style={{'right':'15px'}}>
             <div class='text-center'> 
               <p>Mode automatique</p>
               <input type='submit' class="btn btn-outline-secondary btn-sm position" value='Activer'></input> 
@@ -110,28 +83,75 @@ crossorigin="anonymous"/>
 
           <div class="row justify-content-center">
 
-              <div class="col-4 border border-dark" style={{'paddingLeft':"0", 'paddingRight':"0"}}>
-                  <LightRoom roomId='1' name="Chambre 1" etatlum={etat['Chambre1']}></LightRoom>
-              </div>
+            {/* Chambre 1 */}
 
-              <div class="col-4 border border-dark" style={{'paddingLeft':"0", 'paddingRight':"0"}}>
-                <LightRoom roomId='2' name="Chambre 2" etatlum={etat['Chambre2']}></LightRoom>
+            <div class="col-4 border border-dark" style={{'paddingLeft':"0", 'paddingRight':"0"}}>
+                <LightRoom roomId='1' name="Chambre 1" etatlum={etat['Chambre1']} urlOn='chambre1_on' urlOff='chambre1_off'></LightRoom>
+                <div class="row">
+                <div class="col">
+                  <input type='submit' class="btn btn-outline-secondary btn-sm position" value='Allumer' onClick={() => axios.post(chambre1_lon)}></input>
+                  <input type='submit' class="btn btn-outline-secondary btn-sm position" value='Eteindre' onClick={() => axios.post(chambre2_loff)}></input>
+                </div>
+                <div class="col">
+                  <input type='submit' class="btn btn-outline-secondary btn-sm position" value='Allumer' onClick={() => axios.post(chambre1_von)}></input>
+                  <input type='submit' class="btn btn-outline-secondary btn-sm position" value='Eteindre' onClick={() => axios.post(chambre1_voff)}></input>
+                </div>
               </div>
+            </div>              
+
+            {/* Chambre 2 */}
+
+            <div class="col-4 border border-dark" style={{'paddingLeft':"0", 'paddingRight':"0"}}>
+              <LightRoom roomId='2' name="Chambre 2" etatlum={etat['Chambre2']}></LightRoom>
+              
+              <div class="row">
+                <div class="col">
+                  <input type='submit' class="btn btn-outline-secondary btn-sm position" value='Allumer' onClick={() => axios.post(chambre2_lon)}></input>
+                  <input type='submit' class="btn btn-outline-secondary btn-sm position" value='Eteindre' onClick={() => axios.post(chambre2_loff)}></input>
+                </div>
+                <div class="col">
+                  <input type='submit' class="btn btn-outline-secondary btn-sm position" value='Allumer' onClick={() => axios.post(chambre2_von)}></input>
+                  <input type='submit' class="btn btn-outline-secondary btn-sm position" value='Eteindre' onClick={() => axios.post(chambre2_von)}></input>
+                </div>
+              </div>
+            </div>
 
           </div>
 
           <div class="row justify-content-center">
 
+            {/* Cuisine */}
+
             <div class="col-4 border border-dark" style={{'paddingLeft':"0", 'paddingRight':"0"}}>
               <LightRoom roomId='3' name="Cuisine" etatlum={etat['Cuisine']}></LightRoom>
-            </div>
+              <div class="row">
+                  <div class="col">
+                    <input type='submit' class="btn btn-outline-secondary btn-sm position" value='Allumer' onClick={() => axios.post(cuisine_lon)}></input>
+                    <input type='submit' class="btn btn-outline-secondary btn-sm position" value='Eteindre' onClick={() => axios.post(cuisine_voff)}></input>
+                  </div>
+                  <div class="col">
+                    <input type='submit' class="btn btn-outline-secondary btn-sm position" value='Allumer' onClick={() => axios.post(cuisine_von)}></input>
+                    <input type='submit' class="btn btn-outline-secondary btn-sm position" value='Eteindre' onClick={() => axios.post(cuisine_voff)}></input>
+                  </div>
+              </div>
+            </div>            
 
+            {/* Salon */}
 
             <div class="col-4 border border-dark" style={{'paddingLeft':"0", 'paddingRight':"0"}}>
               <LightRoom roomId='4' name="Salon" etatlum={etat['Salon']}></LightRoom>
-            </div>
-
-
+              <div class="row">
+                  <div class="col">
+                    <input type='submit' class="btn btn-outline-secondary btn-sm position" value='Allumer' onClick={() => axios.post(salon_lon)}></input>
+                    <input type='submit' class="btn btn-outline-secondary btn-sm position" value='Eteindre' onClick={() => axios.post(salon_loff)}></input>
+                  </div>
+                  <div class="col">
+                    <input type='submit' class="btn btn-outline-secondary btn-sm position" value='Allumer' onClick={() => axios.post(salon_von)}></input>
+                    <input type='submit' class="btn btn-outline-secondary btn-sm position" value='Eteindre' onClick={() => axios.post(salon_voff)}></input>
+                  </div>
+              </div>
+            </div>           
+                
           </div>
 
         </div>
--- src/LightRoom.jsx
+++ src/LightRoom.jsx
@@ -4,54 +4,34 @@ import axios from 'axios';
 function LightRoom(props) {
 
     
-    // // Variable pour l'état des volets dans la pièce
+    // Variable pour l'état des volets dans la pièce
 
     var etatvolet = 'on';
-   
+
     return(
 
         <div class='container'>
 
             <div class="row justify-content-center">
 
-            <h1 className="App-piece">{props.name} </h1>
+                <h1 className="App-piece">{props.name} </h1>
 
-            <div class="col">
-                <div class="text-center">
-                    <p> Lumière : {props.etatlum}</p>
-                    <img class="position" src='https://media.istockphoto.com/vectors/light-bulb-with-rays-lighting-electric-lamp-creative-idea-solution-vector-id1149246206?k=20&m=1149246206&s=612x612&w=0&h=yz2GuTngOnZAHYq4wDsAwq0mp20OWlIUSSdmyIU3sRM=' width='150' height='120'></img>
-                </div>
-            </div>
-
-
-            <div class="col">
-                <div class="text-center">                  
-                    <p class="text-center"> Volets : {etatvolet} </p>
-                    <img class="position" src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQf2RlM1U8Neiv0EJ3JBUWg046KssJYF-Qt9Q&usqp=CAU' width='120' height='120'></img>
-                </div>  
-            </div>
-
-            </div>
-            
-            <div class="row justify-content-center"></div>
-
-
-            <div class="row justify-content-center">
                 <div class="col">
                     <div class="text-center">
-                        <input type='submit' class="btn btn-outline-secondary btn-sm position" value='Allumer'></input>
-                        <input type='submit' class="btn btn-outline-secondary btn-sm position" value='Eteindre'></input>
+                        <p> Lumière : {props.etatlum}</p>
+                        <img class="position" src='https://media.istockphoto.com/vectors/light-bulb-with-rays-lighting-electric-lamp-creative-idea-solution-vector-id1149246206?k=20&m=1149246206&s=612x612&w=0&h=yz2GuTngOnZAHYq4wDsAwq0mp20OWlIUSSdmyIU3sRM=' width='150' height='120'></img>
                     </div>
                 </div>
 
+
                 <div class="col">
                     <div class="text-center">                  
-                        <input type='submit' class="btn btn-outline-secondary btn-sm position" value='Ouvrir'></input>
-                        <input type='submit' class="btn btn-outline-secondary btn-sm position" value='Fermer'></input>
+                        <p class="text-center"> Volets : {props.urlOff} </p>
+                        <img class="position" src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQf2RlM1U8Neiv0EJ3JBUWg046KssJYF-Qt9Q&usqp=CAU' width='120' height='120'></img>
                     </div>  
                 </div>
 
-            </div> 
+            </div>
 
         </div>
     )
